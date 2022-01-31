29a2a8b81e243beb5ab4fdcaf10522d1d5919ba5
GenerallissimoF <95148081+GenerallissimoF@users.noreply.github.com>
2022-02-01 00:33:07 +0300

    Initial Commit

---
+++ 777/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  777
+//
+//  Created by Ivan Adoniev on 01.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
